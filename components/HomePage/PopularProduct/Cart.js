f2df6b2c3b2f4c7e81d54bb757f693edd184e795
Esca16 <znh1684@gmail.com>
2023-09-28 12:12:48 +0630

    added fav

---
+++ components/HomePage/PopularProduct/Cart.js
@@ -0,0 +1,11 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+const Cart = () => {
+    return (
+        <>
+            <AiOutlineShoppingCart size={24} className="text-violet-900 cursor-pointer"/>
+        </>
+    );
+}
+
+export default Cart;
